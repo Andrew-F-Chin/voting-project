259b29c006a51297d698d7a40f9ff47036e761ca
Andy Chin <afc@nwmicro.com>
2026-08-20 20:44:09 -0700

    Implemented two voting methods

--- test.py
+++ test.py
@@ -1,2 +1,30 @@
 import numpy as np
 np.array
+
+def first_past_post(b):
+    results = np.zeros_like(b[0])
+    for ballot in b:
+        for canadate in range(len(ballot)):
+            if ballot[canadate] == 1:
+                results[canadate] += 1
+    return(results)
+
+
+def bourda_count(b):
+    results = np.zeros_like(b[0])
+    for ballot in b:
+        for canadate in range(len(ballot)):
+            results[canadate] += ballot[canadate]
+    for canadate in range(len(ballot)):
+        results[canadate] = len(ballot) * (len(b) + 1) - results[canadate]
+    return(results)
+
+test_ballots = [[1, 2, 3],
+                [3, 2, 1],
+                [1, 2, 3]]
+
+first_past_post(test_ballots)
+winners_list = first_past_post(test_ballots)
+bourda_winner = bourda_count(test_ballots)
+
+print(winners_list, bourda_winner)
