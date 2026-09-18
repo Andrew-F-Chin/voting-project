3aee92b81b7c2be0123c3919e82e691cc18b7032
Andrew-F-Chin <afc@nwmicro.com>
2026-09-17 20:23:10 -0700

    Four methods implemented

--- test.py
+++ test.py
@@ -23,25 +23,36 @@ test_ballots = [[1, 2, 3],
                 [3, 2, 1],
                 [1, 2, 3]]
 
-def condorce_method(b):
+def instant_runoff(b):
     results = np.zeros_like(b[0])
-    max_votes = []
+    max_votes = 0
     elim_list = []
     elim_round = 1
-    while max_votes(1) < 50:
-        min = [[0, 0]]
+    while max_votes/len(b) < .5:
+        min = [0, 0]
         for ballot in b:
-            for canadate in range(len(ballot)) and not elim_list:
-                if ballot(canadate) == elim_round:
-                    results(canadate) += 1
+            remainders = [ ix for ix in range(len(ballot)) if ix not in elim_list]
+            for canadate in remainders:
+                if ballot[canadate] == elim_round:
+                    results[canadate] += 1
         for canadate in range(len(results)):
-            if results(canadate) < min(0)(1):
-                min(0)
+            if results[canadate] < min[1]:
+                min =  results[canadate] #finish tie handler and make sure all edge cases are covered
+        for canadate in range(len(results)):
+            if results[canadate] > max_votes:
+                max_votes =  results[canadate] #finish tie handler and make sure all edge cases are covered
+        elim_list.append(min[0])
+    return(results)
+
+def condorcet_method(b):
+    results = np.zeros_like(b[0])
+
 
 
 first_past_post(test_ballots)
 winners_list = first_past_post(test_ballots)
 bourda_winner = bourda_count(test_ballots)
+instant_runoff_winner = instant_runoff(test_ballots)
 
-print(winners_list, bourda_winner)
+print(winners_list, bourda_winner, instant_runoff_winner)
  
